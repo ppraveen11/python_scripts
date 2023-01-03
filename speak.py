83c0413bf260bca1e8899fcf2339a82ef2ad4f62
praveen <ppraveen5974@gmail.com>
2023-01-03 09:15:04 +0530

    fun-update

--- speak.py
+++ speak.py
@@ -1,4 +1,5 @@
-import pyttsx3
-speaker = pyttsx3.init()
-speaker.say("hello praveen")
-speaker.runAndWait()
+def  speak(text):
+	import pyttsx3
+	speaker = pyttsx3.init()
+	speaker.say(text)
+	speaker.runAndWait()
